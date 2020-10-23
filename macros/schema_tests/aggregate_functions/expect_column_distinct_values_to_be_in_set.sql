b500ce97a4bf5c5f94a27e77bf7d525b339ad29d
clausherther <claus@calogica.com>
2020-10-23 10:37:21 -0600

    Add new tests

---
+++ macros/schema_tests/aggregate_functions/expect_column_distinct_values_to_be_in_set.sql
@@ -0,0 +1,47 @@
+{% macro test_expect_column_distinct_values_to_be_in_set(model, values) %}
+
+{% set column_name = kwargs.get('column_name', kwargs.get('field')) %}
+{% set quote_values = kwargs.get('quote', True) %}
+{% set partition_column = kwargs.get('partition_column', kwargs.get('arg')) %}
+{% set partition_filter =  kwargs.get('partition_filter', kwargs.get('arg')) %}
+
+with all_values as (
+
+    select distinct
+        {{ column_name }} as value_field
+
+    from {{ model }}
+    {% if partition_column and partition_filter %}
+    where {{ partition_column }} {{ partition_filter }}
+    {% endif %}
+
+),
+set_values as (
+
+    {% for value in values -%}
+    select 
+        {% if quote_values -%}
+        '{{ value }}'
+        {%- else -%}
+        {{ value }}
+        {%- endif -%} as value_field
+    {% if not loop.last %}union all{% endif %}
+    {% endfor %}
+),
+validation_errors as (
+    -- values from the model that are not in the set
+    select
+        v.value_field
+    from 
+        all_values v
+        left outer join
+        set_values s on v.value_field = s.value_field
+    where
+        v.value_field is null
+
+)
+
+select count(*) as validation_errors
+from validation_errors
+
+{% endmacro %}
---
+++ macros/schema_tests/aggregate_functions/expect_column_distinct_values_to_contain_set.sql
@@ -0,0 +1,47 @@
+{% macro test_expect_column_distinct_values_to_contain_set(model, values) %}
+
+{% set column_name = kwargs.get('column_name', kwargs.get('field')) %}
+{% set quote_values = kwargs.get('quote', True) %}
+{% set partition_column = kwargs.get('partition_column', kwargs.get('arg')) %}
+{% set partition_filter =  kwargs.get('partition_filter', kwargs.get('arg')) %}
+
+with all_values as (
+
+    select distinct
+        {{ column_name }} as value_field
+
+    from {{ model }}
+    {% if partition_column and partition_filter %}
+    where {{ partition_column }} {{ partition_filter }}
+    {% endif %}
+
+),
+set_values as (
+
+    {% for value in values -%}
+    select 
+        {% if quote_values -%}
+        '{{ value }}'
+        {%- else -%}
+        {{ value }}
+        {%- endif -%} as value_field
+    {% if not loop.last %}union all{% endif %}
+    {% endfor %}
+),
+validation_errors as (
+    -- values in set that are not in the list of values from the model
+    select
+        s.value_field
+    from 
+        set_values s
+        left outer join
+        all_values v on s.value_field = v.value_field
+    where
+        v.value_field is null
+
+)
+
+select count(*) as validation_errors
+from validation_errors
+
+{% endmacro %}
