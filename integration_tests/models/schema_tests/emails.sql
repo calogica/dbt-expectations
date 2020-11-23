2fffa55c81bb6e9a781ff26d50491aa3330dd820
clausherther <claus@calogica.com>
2020-11-23 11:02:30 -0700

    Add emails model

---
+++ integration_tests/models/schema_tests/emails.sql
@@ -0,0 +1,20 @@
+
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+
+select 'ab@gmail.com' as email_address, '@[^.]*' as regexp 
+
+union all
+
+select 'ab@mail.com', '@[^.]*' 
+
+union all
+
+select 'abc@gmail.com', '@[^.]*' 
+    
+union all
+
+select 'abc.com@gmail.com', '@[^.]*'
