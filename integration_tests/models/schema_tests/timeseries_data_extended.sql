06754f8aa801a46cb57368c11ccf15d0b6b10ede
clausherther <claus@calogica.com>
2021-03-28 09:42:48 -0600

    Fixes datetime series for Snowflake

--- integration_tests/models/schema_tests/timeseries_data_extended.sql
+++ integration_tests/models/schema_tests/timeseries_data_extended.sql
@@ -3,15 +3,18 @@ with dates as (
     {{ dbt_date.get_base_dates(n_dateparts=12, datepart='month') }}
 
 ),
+row_values as (
+    {{ dbt_utils.generate_series(upper_bound=10) }}
+),
 add_row_values as (
 
     select
         d.date_day,
-        cast(floor(100 * rnd) as {{ dbt_utils.type_int() }}) as row_value
+        cast(floor(100 * r.generated_number) as {{ dbt_utils.type_int() }}) as row_value
     from
         dates d
         cross join
-        unnest(generate_array(1, 10)) as rnd
+        row_values r
 
 ),
 add_logs as (
