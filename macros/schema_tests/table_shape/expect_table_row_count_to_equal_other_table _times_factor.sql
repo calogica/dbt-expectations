bbf593d182bd43c58da98c3ed9d496870402834b
clausherther <claus@calogica.com>
2021-04-19 09:13:18 -0600

    Add ability to set row_condition on row_count tests

--- macros/schema_tests/table_shape/expect_table_row_count_to_equal_other_table _times_factor.sql
+++ macros/schema_tests/table_shape/expect_table_row_count_to_equal_other_table _times_factor.sql
@@ -1,3 +1,6 @@
-{%- macro test_expect_table_row_count_to_equal_other_table_times_factor(model, compare_model, factor) -%}
-{{ dbt_expectations.test_equal_expression(model, "count(*)", compare_model=compare_model, compare_expression="count(*) * " + factor|string, return_difference=True) }}
+{%- macro test_expect_table_row_count_to_equal_other_table_times_factor(model, compare_model, factor, row_condition=None, compare_row_condition=None) -%}
+{{ dbt_expectations.test_expect_table_row_count_to_equal_other_table(model, compare_model, factor,
+    row_condition=row_condition,
+    compare_row_condition=compare_row_condition
+) }}
 {%- endmacro -%}
--- macros/schema_tests/table_shape/expect_table_row_count_to_equal_other_table.sql
+++ macros/schema_tests/table_shape/expect_table_row_count_to_equal_other_table.sql
@@ -1,3 +1,9 @@
-{%- macro test_expect_table_row_count_to_equal_other_table(model, compare_model) -%}
-{{ dbt_expectations.test_equal_expression(model, "count(*)", compare_model=compare_model, return_difference=True) }}
+{%- macro test_expect_table_row_count_to_equal_other_table(model, compare_model, factor=1, row_condition=None, compare_row_condition=None) -%}
+{{ dbt_expectations.test_equal_expression(model, "count(*)",
+    compare_model=compare_model,
+    compare_expression="count(*) * " + factor|string,
+    row_condition=row_condition,
+    compare_row_condition=compare_row_condition,
+    return_difference=True
+) }}
 {%- endmacro -%}
