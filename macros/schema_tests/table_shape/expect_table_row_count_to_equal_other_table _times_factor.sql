e9ab7eecafdc3ee8390230d3f4b22c924e708c59
clausherther <claus@calogica.com>
2021-04-19 09:17:10 -0600

    Set factor parameter more explicitly

--- macros/schema_tests/table_shape/expect_table_row_count_to_equal_other_table _times_factor.sql
+++ macros/schema_tests/table_shape/expect_table_row_count_to_equal_other_table _times_factor.sql
@@ -1,5 +1,6 @@
 {%- macro test_expect_table_row_count_to_equal_other_table_times_factor(model, compare_model, factor, row_condition=None, compare_row_condition=None) -%}
-{{ dbt_expectations.test_expect_table_row_count_to_equal_other_table(model, compare_model, factor,
+{{ dbt_expectations.test_expect_table_row_count_to_equal_other_table(model, compare_model,
+    factor=factor,
     row_condition=row_condition,
     compare_row_condition=compare_row_condition
 ) }}
