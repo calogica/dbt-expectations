f1ac162733f5eaadb644c9998705ecf9d901a784
clausherther <claus@calogica.com>
2021-05-28 12:34:01 -0700

    Update between tests to support strictly

--- macros/schema_tests/aggregate_functions/expect_column_median_to_be_between.sql
+++ macros/schema_tests/aggregate_functions/expect_column_median_to_be_between.sql
@@ -2,7 +2,8 @@
                                                     min_value=None,
                                                     max_value=None,
                                                     group_by=None,
-                                                    row_condition=None
+                                                    row_condition=None,
+                                                    strictly=False
                                                     ) %}
 
 {% set expression %}
@@ -13,6 +14,7 @@
                                         min_value=min_value,
                                         max_value=max_value,
                                         group_by_columns=group_by,
-                                        row_condition=row_condition
+                                        row_condition=row_condition,
+                                        strictly=strictly
                                         ) }}
 {% endmacro %}
