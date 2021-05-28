94e5715e4d8f32c16fa0f8455ba614598d48b260
clausherther <claus@calogica.com>
2021-05-28 12:34:01 -0700

    Update between tests to support strictly

--- macros/schema_tests/aggregate_functions/expect_column_max_to_be_between.sql
+++ macros/schema_tests/aggregate_functions/expect_column_max_to_be_between.sql
@@ -2,7 +2,8 @@
                                                 min_value=None,
                                                 max_value=None,
                                                 group_by=None,
-                                                row_condition=None
+                                                row_condition=None,
+                                                strictly=False
                                                 ) %}
 {% set expression %}
 max({{ column_name }})
@@ -12,6 +13,7 @@ max({{ column_name }})
                                         min_value=min_value,
                                         max_value=max_value,
                                         group_by_columns=group_by,
-                                        row_condition=row_condition
+                                        row_condition=row_condition,
+                                        strictly=strictly
                                         ) }}
 {% endmacro %}
--- macros/schema_tests/column_values_basic/expect_column_values_to_be_between.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_between.sql
@@ -1,7 +1,8 @@
 {% macro test_expect_column_values_to_be_between(model, column_name,
                                                    min_value=None,
                                                    max_value=None,
-                                                   row_condition=None
+                                                   row_condition=None,
+                                                   strictly=False
                                                    ) %}
 
 {% set expression %}
@@ -13,7 +14,8 @@
                                         min_value=min_value,
                                         max_value=max_value,
                                         group_by_columns=None,
-                                        row_condition=row_condition
+                                        row_condition=row_condition,
+                                        strictly=strictly
                                         ) }}
 
 
