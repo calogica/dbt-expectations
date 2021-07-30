9a0b5514209b7bfa3c2ed5402407d64c158fd5fe
Eduardo Bizarro <edbizarro@gmail.com>
2021-07-30 16:12:47 -0300

    include row_condition on expect_grouped_row_values_to_have_recent_data test

--- macros/schema_tests/table_shape/expect_grouped_row_values_to_have_recent_data.sql
+++ macros/schema_tests/table_shape/expect_grouped_row_values_to_have_recent_data.sql
@@ -1,10 +1,15 @@
-{% test expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval) %}
+{% test expect_grouped_row_values_to_have_recent_data(model,
+                                                            group_by,
+                                                            timestamp_column,
+                                                            datepart,
+                                                            interval,
+                                                            row_condition=None) %}
 
- {{ adapter.dispatch('test_expect_grouped_row_values_to_have_recent_data', 'dbt_expectations') (model, group_by, timestamp_column, datepart, interval) }}
+ {{ adapter.dispatch('test_expect_grouped_row_values_to_have_recent_data', 'dbt_expectations') (model, group_by, timestamp_column, datepart, interval, row_condition) }}
 
 {% endtest %}
 
-{% macro default__test_expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval) %}
+{% macro default__test_expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval, row_condition) %}
 with latest_grouped_timestamps as (
 
     select
@@ -12,9 +17,14 @@ with latest_grouped_timestamps as (
         {{ g }},
         {%- endfor %}
         max({{ timestamp_column }}) as latest_timestamp_column
-    from
-        {{ model }}
+    from {{ model }}
+
+    {% if row_condition %}
+    where {{ row_condition }}
+    {% endif %}
+
     {{ dbt_utils.group_by(group_by | length )}}
+
 
 ),
 validation_errors as (
