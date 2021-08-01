f7a0d009606dbafd420ed50bc7ec918cd0e65be2
clausherther <claus@calogica.com>
2021-08-01 07:18:28 -0700

    Add more robust logic to support row_condition

--- macros/schema_tests/table_shape/expect_grouped_row_values_to_have_recent_data.sql
+++ macros/schema_tests/table_shape/expect_grouped_row_values_to_have_recent_data.sql
@@ -1,24 +1,35 @@
 {% test expect_grouped_row_values_to_have_recent_data(model,
-                                                            group_by,
-                                                            timestamp_column,
-                                                            datepart,
-                                                            interval,
-                                                            row_condition=None) %}
+                                                        group_by,
+                                                        timestamp_column,
+                                                        datepart,
+                                                        interval,
+                                                        row_condition=None) %}
 
- {{ adapter.dispatch('test_expect_grouped_row_values_to_have_recent_data', 'dbt_expectations') (model, group_by, timestamp_column, datepart, interval, row_condition) }}
+ {{ adapter.dispatch('test_expect_grouped_row_values_to_have_recent_data', 'dbt_expectations') (model,
+                                                                                                group_by,
+                                                                                                timestamp_column,
+                                                                                                datepart,
+                                                                                                interval,
+                                                                                                row_condition) }}
 
 {% endtest %}
 
-{% macro default__test_expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval, row_condition) %}
+{% macro default__test_expect_grouped_row_values_to_have_recent_data(model,
+                                                                        group_by,
+                                                                        timestamp_column,
+                                                                        datepart,
+                                                                        interval,
+                                                                        row_condition) %}
 with latest_grouped_timestamps as (
 
     select
         {%- for g in group_by %}
         {{ g }},
         {%- endfor %}
+        max(1) as join_key,
         max({{ timestamp_column }}) as latest_timestamp_column
-    from {{ model }}
-
+    from
+        {{ model }}
     {% if row_condition %}
     where {{ row_condition }}
     {% endif %}
@@ -27,13 +38,33 @@ with latest_grouped_timestamps as (
 
 
 ),
-validation_errors as (
+total_row_counts as (
+    select max(1) as join_key, count(*) as row_count from latest_grouped_timestamps
+),
+outdated_grouped_timestamps as (
 
     select *
     from
-        latest_grouped_timestamps
+        latest_grouped_timestamps t
     where
         latest_timestamp_column < {{ dbt_utils.dateadd(datepart, interval * -1, dbt_date.now()) }}
+
+),
+validation_errors as (
+
+    select
+        r.row_count,
+        t.*
+    from
+        total_row_counts r
+        left join
+        outdated_grouped_timestamps t
+        on r.join_key = t.join_key
+    where
+        -- fail if either no rows were returned due to row_condition,
+        -- or the recency test returned failed rows
+        r.row_count = 0
+        or t.join_key is not null
 
 )
 select * from validation_errors
