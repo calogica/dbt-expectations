9c075f19f4f8a00b0cd7f107c729817fc1c8caf0
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2021-11-10 11:17:16 -0500

    Feature/add interval arg to values every n datepart (#110)
    * add `interval` argument
    
    for checking presence every n-date_parts instead of every date_part
    
    * update docs for explaining new `interval` arg
    
    [expect_row_values_to_have_data_for_every_n_datepart](https://github.com/calogica/dbt-expectations/tree/0.4.2#expect_row_values_to_have_data_for_every_n_datepart)
    
    * expect_table_columns_to_match_ordered_list: refactor row_number to use loop.index (#112)
    
    * Fixes #111 - refactor row_number to use loop.index
    
    * Update CHANGELOG
    
    * handle data types for `mod` and incorporate windowing
    
    This test will handle the mod function, which only takes integer arguments, more stably. It also aggregates row counts across intervals when joining on the date spine to correctly detect data presence in the target model
    
    update conditions based on interval
    
    update styling
    
    update styling
    
    * Add support for dbt 0.21 (#116)
    
    * Update README.md
    
    * remove unintentional styling changes
    
    * Fix expect_row_values_to_have_data_for_every_n_datepart errors when both start and end dates are set (#115)
    
    * fix none when both test dates are set
    
    * Add support for dbt 0.21 (#116)
    
    * Update README.md
    
    * fix none when both test dates are set
    
    * Update README
    
    Co-authored-by: Claus Herther <claus@calogica.com>
    
    * add `interval` argument
    
    for checking presence every n-date_parts instead of every date_part
    
    * update docs for explaining new `interval` arg
    
    [expect_row_values_to_have_data_for_every_n_datepart](https://github.com/calogica/dbt-expectations/tree/0.4.2#expect_row_values_to_have_data_for_every_n_datepart)
    
    * handle data types for `mod` and incorporate windowing
    
    This test will handle the mod function, which only takes integer arguments, more stably. It also aggregates row counts across intervals when joining on the date spine to correctly detect data presence in the target model
    
    update conditions based on interval
    
    update styling
    
    update styling
    
    * remove unintentional styling changes
    
    * Change datepart param and fix formatting
    
    * Reformat join to match prior style
    
    * simplify tie-out of model data to spine with interval truncation
    
    the condition added to the model_data CTE is meant to emulate (kind of) Snowflake's [`TIME_SLICE`](https://docs.snowflake.com/en/sql-reference/functions/time_slice.html), which should allow exact matches to the base_dates CTE for better time bucketing
    
    * Add schema test
    
    * replace calls to subquery with calls directly to columns in `model_data` CTE
    
    * add comments/examples for new interval additions
    
    Co-authored-by: Claus Herther <claus@calogica.com>
    Co-authored-by: Jeremy Yeo <jeremyyeo@users.noreply.github.com>

--- macros/schema_tests/distributional/expect_row_values_to_have_data_for_every_n_datepart.sql
+++ macros/schema_tests/distributional/expect_row_values_to_have_data_for_every_n_datepart.sql
@@ -1,6 +1,7 @@
 {%- test expect_row_values_to_have_data_for_every_n_datepart(model,
                                                                     date_col,
                                                                     date_part="day",
+                                                                    interval=None,
                                                                     row_condition=None,
                                                                     test_start_date=None,
                                                                     test_end_date=None) -%}
@@ -42,12 +43,53 @@
 with base_dates as (
 
     {{ dbt_date.get_base_dates(start_date=start_date, end_date=end_date, datepart=date_part) }}
+    {% if interval %}
+    {# 
+        Filter the date spine created above down to the interval granularity using a modulo operation.
+        The number of date_parts after the start_date divided by the integer interval will produce no remainder for the desired intervals, 
+        e.g. for 2-day interval from a starting Jan 1, 2020:
+            params: start_date = '2020-01-01', date_part = 'day', interval = 2
+            date spine created above: [2020-01-01, 2020-01-02, 2020-01-03, 2020-01-04, 2020-01-05, ...]
+            The first parameter to the `mod` function would be the number of days between the start_date and the spine date, i.e. [0, 1, 2, 3, 4 ...]
+            The second parameter to the `mod` function would be the integer interval, i.e. 2
+            This modulo operation produces the following remainders: [0, 1, 0, 1, 0, ...]
+            Filtering the spine only where this remainder == 0 will return a spine with every other day as desired, i.e. [2020-01-01, 2020-01-03, 2020-01-05, ...]
+    #}
+    where mod(
+            cast({{ dbt_utils.datediff("'" ~ start_date ~ "'", 'date_' ~ date_part, date_part) }} as {{ dbt_utils.type_int() }}),
+            cast({{interval}} as {{ dbt_utils.type_int() }})
+        ) = 0
+    {% endif %}
 
 ),
 model_data as (
 
     select
+    {% if not interval %}
+
         cast({{ dbt_utils.date_trunc(date_part, date_col) }} as {{ dbt_expectations.type_datetime() }}) as date_{{ date_part }},
+
+    {% else %}
+        {# 
+            Use a modulo operator to determine the number of intervals that a date_col is away from the interval-date spine 
+            and subtracts that amount to effectively slice each date_col record into its corresponding spine bucket,
+            e.g. given a date_col of with records [2020-01-01, 2020-01-02, 2020-01-03, 2020-01-11, 2020-01-12]
+                if we want to slice these dates into their 2-day buckets starting Jan 1, 2020 (start_date = '2020-01-01', date_part='day', interval=2),
+                the modulo operation described above will produce these remainders: [0, 1, 0, 0, 1]
+                subtracting that number of days from the observations will produce records [2020-01-01, 2020-01-01, 2020-01-03, 2020-01-11, 2020-01-11],
+                all of which align with records from the interval-date spine
+        #}
+        {{dbt_utils.dateadd(
+            date_part, 
+            "mod(
+                cast(" ~ dbt_utils.datediff("'" ~ start_date ~ "'", date_col, date_part) ~ " as " ~ dbt_utils.type_int() ~ " ),
+                cast(" ~ interval ~ " as  " ~ dbt_utils.type_int() ~ " )
+            ) * (-1)", 
+            "cast( " ~ dbt_utils.date_trunc(date_part, date_col) ~ " as  " ~ dbt_expectations.type_datetime() ~ ")"
+        )}} as date_{{ date_part }},
+            
+    {% endif %}
+    
         count(*) as row_cnt
     from
         {{ model }} f
@@ -58,6 +100,7 @@ model_data as (
         date_{{date_part}}
 
 ),
+
 final as (
 
     select
