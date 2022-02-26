a3b7f294174d0aad551c63a925a0723f6c8e7257
lucasthenoob <lucaslarbodiere@gmail.com>
2022-02-26 10:19:39 -0500

    Added partition by parameter

--- macros/schema_tests/column_values_basic/expect_column_values_to_be_decreasing.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_decreasing.sql
@@ -1,16 +1,22 @@
 {% test expect_column_values_to_be_decreasing(model, column_name,
                                                    sort_column=None,
                                                    strictly=True,
-                                                   row_condition=None) %}
+                                                   row_condition=None,
+                                                   partition_columns=None) %}
 
 {%- set sort_column = column_name if not sort_column else sort_column -%}
 {%- set operator = "<" if strictly else "<=" %}
+{%- set partition_columns_list = partition_columns.split(',') %}
 with all_values as (
 
     select
         {{ sort_column }} as sort_column,
         {{ column_name }} as value_field
-
+        {%- if partition_columns -%}
+            {%- for columns in partition_columns_list -%}
+        , {{ columns }}
+            {%- endfor -%}
+        {%- endif %}
     from {{ model }}
     {% if row_condition %}
     where {{ row_condition }}
@@ -22,7 +28,12 @@ add_lag_values as (
     select
         sort_column,
         value_field,
-        lag(value_field) over(order by sort_column) as value_field_lag
+        lag(value_field) over
+            {%- if not partition_columns -%}
+                (order by sort_column)
+            {%- else -%}
+                (partition by {{ partition_columns }} order by sort_column)
+            {%- endif -%}    as value_field_lag    
     from
         all_values
 
--- macros/schema_tests/column_values_basic/expect_column_values_to_be_increasing.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_increasing.sql
@@ -1,34 +1,40 @@
 {% test expect_column_values_to_be_increasing(model, column_name,
                                                    sort_column=None,
                                                    strictly=True,
-                                                   row_condition=None) %}
+                                                   row_condition=None,
+                                                   partition_columns=None) %}
 
 {%- set sort_column = column_name if not sort_column else sort_column -%}
 {%- set operator = ">" if strictly else ">=" %}
+{%- set partition_columns_list = partition_columns.split(',') %}
 with all_values as (
-
     select
         {{ sort_column }} as sort_column,
         {{ column_name }} as value_field
-
+        {%- if partition_columns -%}
+            {%- for columns in partition_columns_list -%}
+        , {{ columns }}
+            {%- endfor -%}
+        {%- endif %}
     from {{ model }}
     {% if row_condition %}
     where {{ row_condition }}
     {% endif %}
-
 ),
 add_lag_values as (
-
     select
         sort_column,
         value_field,
-        lag(value_field) over(order by sort_column) as value_field_lag
+        lag(value_field) over
+            {%- if not partition_columns -%}
+                (order by sort_column)
+            {%- else -%}
+                (partition by {{ partition_columns }} order by sort_column)
+            {%- endif -%}    as value_field_lag
     from
         all_values
-
 ),
 validation_errors as (
-
     select
         *
     from
@@ -37,7 +43,6 @@ validation_errors as (
         value_field_lag is not null
         and
         not (value_field {{ operator }} value_field_lag)
-
 )
 select *
 from validation_errors
