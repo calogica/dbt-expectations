e7d011c24a5bdfb5bbe0f96236e5aaec606368db
clausherther <claus@calogica.com>
2022-03-09 08:04:50 -0800

    Add datatypes

--- macros/utils/datatypes.sql
+++ macros/utils/datatypes.sql
@@ -1,12 +1,25 @@
+{# timestamp  -------------------------------------------------     #}
+{%- macro type_timestamp() -%}
+  {{ return(adapter.dispatch('type_timestamp', 'dbt_expectations')()) }}
+{%- endmacro -%}
+
+{% macro default__type_timestamp() -%}
+    timestamp
+{%- endmacro %}
+
+{% macro snowflake__type_timestamp() -%}
+    timestamp_ntz
+{%- endmacro %}
+
 {% macro postgres__type_timestamp() -%}
     timestamp without time zone
 {%- endmacro %}
 
+{# datetime  -------------------------------------------------     #}
 
-
-{%- macro type_datetime() -%}
+{% macro type_datetime() -%}
   {{ return(adapter.dispatch('type_datetime', 'dbt_expectations')()) }}
-{%- endmacro -%}
+{%- endmacro %}
 
 {% macro default__type_datetime() -%}
     datetime
