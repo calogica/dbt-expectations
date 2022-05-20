adefcb5f285f2fa3d9de7594125ae05745d1560c
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-05-20 09:46:27 +0100

    Correct typo

--- macros/schema_tests/table_shape/_get_column_list.sql
+++ macros/schema_tests/table_shape/_get_column_list.sql
@@ -1,5 +1,5 @@
 {%- macro _get_column_list(model, transform="upper") -%}
-    {%- if (model is mapping and obj.get('metadata', {}).get('type', '').endswith('Relation')) -%}
+    {%- if (model is mapping and model.get('metadata', {}).get('type', '').endswith('Relation')) -%}
         {%- set relation_columns = adapter.get_columns_in_relation(model) -%}
     {%- else -%}
         {%- set relation_columns = adapter.get_columns_in_query(model) -%}
