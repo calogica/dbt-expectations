c99b2a82733a6879c8c72b850100a54d689ba4af
Claus Herther <claus@calogica.com>
2022-09-14 07:16:40 -0700

    Make raw regex string optional

--- macros/regex/regexp_instr.sql
+++ macros/regex/regexp_instr.sql
@@ -1,31 +1,33 @@
-{% macro regexp_instr(source_value, regexp, position=1, occurrence=1) %}
+{% macro regexp_instr(source_value, regexp, position=1, occurrence=1, is_raw=False) %}
 
     {{ adapter.dispatch('regexp_instr', 'dbt_expectations')(
-        source_value, regexp, position, occurrence
+        source_value, regexp, position, occurrence, is_raw
     ) }}
 
 {% endmacro %}
 
-{% macro default__regexp_instr(source_value, regexp, position, occurrence) %}
+{% macro default__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
 regexp_instr({{ source_value }}, '{{ regexp }}', {{ position }}, {{ occurrence }})
 {% endmacro %}
 
 {# Snowflake uses $$...$$ to escape raw strings #}
-{% macro snowflake__regexp_instr(source_value, regexp, position, occurrence) %}
-regexp_instr({{ source_value }}, $${{ regexp }}$$, {{ position }}, {{ occurrence }})
+{% macro snowflake__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
+{%- set regexp = "$$" ~ regexp ~ "$$" if is_raw else "'" ~ regexp ~ "'" -%}
+regexp_instr({{ source_value }}, {{ regexp }}, {{ position }}, {{ occurrence }})
 {% endmacro %}
 
 {# BigQuery uses "r" to escape raw strings #}
-{% macro bigquery__regexp_instr(source_value, regexp, position, occurrence) %}
-regexp_instr({{ source_value }}, r'{{ regexp }}', {{ position }}, {{ occurrence }})
+{% macro bigquery__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
+{%- set regexp = "r'" ~ regexp ~ "'" if is_raw else "'" ~ regexp ~ "'" -%}
+regexp_instr({{ source_value }}, {{ regexp }}, {{ position }}, {{ occurrence }})
 {% endmacro %}
 
 {# Postgres does not need to escape raw strings #}
-{% macro postgres__regexp_instr(source_value, regexp, position, occurrence) %}
+{% macro postgres__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
 array_length((select regexp_matches({{ source_value }}, '{{ regexp }}')), 1)
 {% endmacro %}
 
 {# Unclear what Redshift does to escape raw strings #}
-{% macro redshift__regexp_instr(source_value, regexp, position, occurrence) %}
+{% macro redshift__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
 regexp_instr({{ source_value }}, '{{ regexp }}', {{ position }}, {{ occurrence }})
 {% endmacro %}
--- macros/schema_tests/string_matching/expect_column_values_to_not_match_regex.sql
+++ macros/schema_tests/string_matching/expect_column_values_to_not_match_regex.sql
@@ -1,10 +1,11 @@
 {% test expect_column_values_to_not_match_regex(model, column_name,
                                                     regex,
-                                                    row_condition=None
+                                                    row_condition=None,
+                                                    is_raw=False
                                                     ) %}
 
 {% set expression %}
-{{ dbt_expectations.regexp_instr(column_name, regex) }} = 0
+{{ dbt_expectations.regexp_instr(column_name, regex, is_raw=is_raw) }} = 0
 {% endset %}
 
 {{ dbt_expectations.expression_is_true(model,
