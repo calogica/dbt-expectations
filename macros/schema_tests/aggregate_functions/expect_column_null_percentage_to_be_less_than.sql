b9161f4e34ea4047eb2f0297fdc21e9b2f626e01
danhphan <danh.phan.mq@gmail.com>
2023-02-09 16:16:55 +1100

    Update expect_column_null_percentage_to_be_less_than.sql

--- macros/schema_tests/aggregate_functions/expect_column_null_percentage_to_be_less_than.sql
+++ macros/schema_tests/aggregate_functions/expect_column_null_percentage_to_be_less_than.sql
@@ -1,11 +1,11 @@
-{% test expect_column_distinct_count_to_be_less_than(model,
+{% test expect_column_null_percentage_to_be_less_than(model,
                                                        column_name,
                                                        value,
                                                        group_by=None,
                                                        row_condition=None
                                                        ) %}
 {% set expression %}
-count( {{ column_name }} ) / count(*) < {{ value }}
+( count(*) - count( {{ column_name }} ) ) * 1.0 / count(*)  < {{ value }} 
 {% endset %}
 {{ dbt_expectations.expression_is_true(model,
                                         expression=expression,
