cb19d6a2c3ee93aa996ff4cfb5cb62bdd8de9965
Robbert Hofman <117672004+RobbertDM@users.noreply.github.com>
2023-03-27 21:29:11 +0200

    escape quotes in expect_column_values_to_be_in_type_list.sql (#251)

--- macros/schema_tests/column_values_basic/expect_column_values_to_be_in_type_list.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_in_type_list.sql
@@ -8,7 +8,7 @@
 
         {% for column in columns_in_relation %}
         select
-            cast('{{ column.name | upper }}' as {{ dbt.type_string() }}) as relation_column,
+            cast('{{ escape_single_quotes(column.name | upper) }}' as {{ dbt.type_string() }}) as relation_column,
             cast('{{ column.dtype | upper }}' as {{ dbt.type_string() }}) as relation_column_type
         {% if not loop.last %}union all{% endif %}
         {% endfor %}
