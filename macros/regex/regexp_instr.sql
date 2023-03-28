80d44e8eed3f1af7fbee51e9c0bcd2e3e937655d
lookslikeitsnot <cedr2009@gmail.com>
2023-03-28 23:03:52 +0200

    added support for regex flags for BigQuery

--- macros/regex/regexp_instr.sql
+++ macros/regex/regexp_instr.sql
@@ -27,9 +27,9 @@ regexp_instr({{ source_value }}, {{ regexp }}, {{ position }}, {{ occurrence }},
 {# BigQuery uses "r" to escape raw strings #}
 {% macro bigquery__regexp_instr(source_value, regexp, position, occurrence, is_raw, flags) %}
 {% if flags %}
-    {{ exceptions.warn(
-            "The flags option is not supported for BigQuery and is being ignored."
-    ) }}
+    {{ dbt_expectations._validate_re2_flags(flags) }}
+    {# BigQuery prepends "(?flags)" to set flags for current group #}
+    {%- set regexp = "(?" ~ flags ~ ")" ~ regexp -%}
 {% endif %}
 {%- set regexp = "r'" ~ regexp ~ "'" if is_raw else "'" ~ regexp ~ "'" -%}
 regexp_instr({{ source_value }}, {{ regexp }}, {{ position }}, {{ occurrence }})
@@ -50,9 +50,31 @@ regexp_instr({{ source_value }}, '{{ regexp }}', {{ position }}, {{ occurrence }
 {% macro _validate_flags(flags, alphabet) %}
 {% for flag in flags %}
     {% if flag not in alphabet %}
-    {{ exceptions.raise_compiler_error(
+    {# Using raise_compiler_error causes disabled tests with invalid flags to fail compilation #}
+    {{ exceptions.warn(
         "flag " ~ flag ~ " not in list of allowed flags for this adapter: " ~ alphabet | join(", ")
     ) }}
     {% endif %}
 {% endfor %}
+{% endmacro %}
+
+{# Re2 requires specific flag validation because of its clear flag operator #}
+{% macro _validate_re2_flags(flags) %}
+{# Re2 supports following flags: #}
+{# i  :  case-insensitive (default fault) #}
+{# m  :  multi-line mode: ^ and $ match begin/end line in addition to begin/end text (default false) #}
+{# s  :  let . match \n (default false) #}
+{# U  :  ungreedy: swap meaning of x* and x*?, x+ and x+?, etc (default false) #}
+{# Flag syntax is xyz (set) or -xyz (clear) or xy-z (set xy, clear z).  #}
+
+{# Regex explanation: do not allow consecutive dashes, accept all re2 flags and clear operator, do not end with a dash  #}
+{% set re2_flags_pattern = '^(?!.*--)[-imsU]*(?<!-)$' %}
+{% set re = modules.re %}
+{% set is_match = re.match(re2_flags_pattern, flags) %}
+{% if not is_match %}
+    {# Using raise_compiler_error causes disabled tests with invalid flags to fail compilation #}
+    {{ exceptions.warn(
+        "flags " ~ flags ~ " isn't a valid re2 flag pattern" 
+    ) }}
+{% endif %}
 {% endmacro %}
