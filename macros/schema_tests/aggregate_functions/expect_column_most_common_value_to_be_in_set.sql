db855a5e5c40f0750806e4da8e1c1d40652e2e3b
lookslikeitsnot <cedr2009@gmail.com>
2023-05-17 17:00:16 +0200

    add ties_okay and fix ties handling

--- macros/schema_tests/aggregate_functions/expect_column_most_common_value_to_be_in_set.sql
+++ macros/schema_tests/aggregate_functions/expect_column_most_common_value_to_be_in_set.sql
@@ -4,11 +4,12 @@
                                                        top_n,
                                                        quote_values=True,
                                                        data_type="decimal",
-                                                       row_condition=None
+                                                       row_condition=None,
+                                                       ties_okay=False
                                                        ) -%}
 
     {{ adapter.dispatch('test_expect_column_most_common_value_to_be_in_set', 'dbt_expectations') (
-            model, column_name, value_set, top_n, quote_values, data_type, row_condition
+            model, column_name, value_set, top_n, quote_values, data_type, row_condition, ties_okay
         ) }}
 
 {%- endtest %}
@@ -19,7 +20,8 @@
                                                                       top_n,
                                                                       quote_values,
                                                                       data_type,
-                                                                      row_condition
+                                                                      row_condition,
+                                                                      ties_okay
                                                                       ) %}
 
 with value_counts as (
@@ -48,7 +50,7 @@ value_counts_ranked as (
 
     select
         *,
-        row_number() over(order by value_count desc) as value_count_rank
+        rank() over(order by value_count desc) as value_count_rank
     from
         value_counts
 
@@ -83,15 +85,23 @@ unique_set_values as (
         set_values
 
 ),
-validation_errors as (
-    -- values from the model that are not in the set
+most_common_values_not_in_set as (
     select
         value_field
     from
         value_count_top_n
     where
         value_field not in (select value_field from unique_set_values)
-
+),
+validation_errors as (
+    {% if ties_okay -%}
+    select mcvnis.* from most_common_values_not_in_set mcvnis
+    , (select count(*) as cnt from most_common_values_not_in_set) as most_common_values_not_in_set_cnt
+    , (select count(*) as cnt from value_count_top_n) as most_common_values_cnt
+    where most_common_values_not_in_set_cnt.cnt >= most_common_values_cnt.cnt
+    {%- else -%}
+    select * from most_common_values_not_in_set
+    {%- endif -%}
 )
 
 select *
