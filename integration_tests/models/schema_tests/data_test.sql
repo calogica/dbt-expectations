269d5a2c2cff5e2d7500ee8e99ca763542d59555
Claus Herther <claus@calogica.com>
2023-05-18 18:44:54 -0700

    Change to use built in type macro

--- integration_tests/models/schema_tests/data_test.sql
+++ integration_tests/models/schema_tests/data_test.sql
@@ -1,8 +1,8 @@
 select
     1 as idx,
     '2020-10-21' as date_col,
-    cast(0 as {{ dbt.type_float() }}) as col_numeric_a,
-    cast(1 as {{ dbt.type_float() }}) as col_numeric_b,
+    cast(0 as {{ dbt.type_numeric() }}) as col_numeric_a,
+    cast(1 as {{ dbt.type_numeric() }}) as col_numeric_b,
     'a' as col_string_a,
     'b' as col_string_b,
     cast(null as {{ dbt.type_string() }}) as col_null,
@@ -13,8 +13,8 @@ union all
 select
     2 as idx,
     '2020-10-22' as date_col,
-    cast(1 as {{ dbt.type_float() }}) as col_numeric_a,
-    cast(0 as {{ dbt.type_float() }}) as col_numeric_b,
+    cast(1 as {{ dbt.type_numeric() }}) as col_numeric_a,
+    cast(0 as {{ dbt.type_numeric() }}) as col_numeric_b,
     'b' as col_string_a,
     'ab' as col_string_b,
     null as col_null,
@@ -25,8 +25,8 @@ union all
 select
     3 as idx,
     '2020-10-23' as date_col,
-    cast(0.5 as {{ dbt.type_float() }}) as col_numeric_a,
-    cast(0.5 as {{ dbt.type_float() }}) as col_numeric_b,
+    cast(0.5 as {{ dbt.type_numeric() }}) as col_numeric_a,
+    cast(0.5 as {{ dbt.type_numeric() }}) as col_numeric_b,
     'c' as col_string_a,
     'abc' as col_string_b,
     null as col_null,
@@ -37,8 +37,8 @@ union all
 select
     4 as idx,
     '2020-10-23' as date_col,
-    cast(0.5 as {{ dbt.type_float() }}) as col_numeric_a,
-    cast(0.5 as {{ dbt.type_float() }}) as col_numeric_b,
+    cast(0.5 as {{ dbt.type_numeric() }}) as col_numeric_a,
+    cast(0.5 as {{ dbt.type_numeric() }}) as col_numeric_b,
     'c' as col_string_a,
     'abcd' as col_string_b,
     null as col_null,
