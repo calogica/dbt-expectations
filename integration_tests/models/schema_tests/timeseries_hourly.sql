4349617dad0ef8185fff9355a5e83d9af27653a3
Claus Herther <claus@calogica.com>
2023-08-06 15:42:26 -0700

    Fix integration test

--- integration_tests/models/schema_tests/timeseries_hourly.sql
+++ integration_tests/models/schema_tests/timeseries_hourly.sql
@@ -1,9 +1,9 @@
-{% set now = modules.datetime.datetime.now().replace(minute=0, hour=0, second=0, microsecond=0) %}
-{% set start_date = (now - modules.datetime.timedelta(10)) %}
+{% set end_date = modules.datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) %}
+{% set start_date = (end_date - modules.datetime.timedelta(days=10)) %}
 
 {{ dbt_date.get_base_dates(
         start_date=start_date,
-        end_date=now,
+        end_date=end_date,
         datepart="hour"
     )
 }}
