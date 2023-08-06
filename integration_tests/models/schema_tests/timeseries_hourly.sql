40bdc2e81d04cb143d2033d855eec95055213a63
Claus Herther <claus@calogica.com>
2023-08-06 12:31:03 -0700

    Update time format

--- integration_tests/models/schema_tests/timeseries_hourly.sql
+++ integration_tests/models/schema_tests/timeseries_hourly.sql
@@ -1,9 +1,9 @@
 {% set now = modules.datetime.datetime.now().replace(minute=0, hour=0, second=0, microsecond=0) %}
-{% set start_date = (now - modules.datetime.timedelta(10)).isoformat() %}
+{% set start_date = (now - modules.datetime.timedelta(10)) %}
 
 {{ dbt_date.get_base_dates(
         start_date=start_date,
-        end_date=now.isoformat(),
+        end_date=now,
         datepart="hour"
     )
 }}
