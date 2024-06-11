5915d08e1960864f6875a4947104d1ea260bb729
kgnelnet <166437483+kgnelnet@users.noreply.github.com>
2024-06-11 11:14:02 -0600

    Update expression_is_true.sql
    Adding context query var

--- macros/schema_tests/_generalized/expression_is_true.sql
+++ macros/schema_tests/_generalized/expression_is_true.sql
@@ -2,10 +2,11 @@
                                  expression,
                                  test_condition="= true",
                                  group_by_columns=None,
-                                 row_condition=None
+                                 row_condition=None,
+                                 query_context=None
                                  ) %}
 
-    {{ dbt_expectations.expression_is_true(model, expression, test_condition, group_by_columns, row_condition) }}
+    {{ dbt_expectations.expression_is_true(model, expression, test_condition, group_by_columns, row_condition, query_context) }}
 
 {% endtest %}
 
@@ -13,14 +14,18 @@
                                  expression,
                                  test_condition="= true",
                                  group_by_columns=None,
-                                 row_condition=None
+                                 row_condition=None,
+                                 query_context=None
                                  ) %}
-    {{ adapter.dispatch('expression_is_true', 'dbt_expectations') (model, expression, test_condition, group_by_columns, row_condition) }}
+    {{ adapter.dispatch('expression_is_true', 'dbt_expectations') (model, expression, test_condition, group_by_columns, row_condition, query_context) }}
 {%- endmacro %}
 
-{% macro default__expression_is_true(model, expression, test_condition, group_by_columns, row_condition) -%}
+{% macro default__expression_is_true(model, expression, test_condition, group_by_columns, row_condition, query_context) -%}
 with grouped_expression as (
     select
+        {% if query_context %}
+           {{ query_context }},
+        {% endif %}
         {% if group_by_columns %}
         {% for group_by_column in group_by_columns -%}
         {{ group_by_column }} as col_{{ loop.index }},
