59cbee7b11faf3e1a9dc4f9a8b92a89b35c1c957
kgnelnet <166437483+kgnelnet@users.noreply.github.com>
2024-06-11 12:28:42 -0600

    Update expect_column_values_to_be_unique.sql
    Reverting query context var addition

--- macros/schema_tests/column_values_basic/expect_column_values_to_be_unique.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_unique.sql
@@ -1,7 +1,6 @@
-{% test expect_column_values_to_be_unique(model, column_name, row_condition=None, query_context=None) %}
+{% test expect_column_values_to_be_unique(model, column_name, row_condition=None) %}
 {{ dbt_expectations.test_expect_compound_columns_to_be_unique(model, 
                                                                 [column_name], 
-                                                                row_condition=row_condition,
-                                                                query_context=query_context
+                                                                row_condition=row_condition
                                                                 ) }}
 {% endtest %}
