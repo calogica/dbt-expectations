8fb229143508c1346ba4ea0cca2c4b56924d8064
kgnelnet <166437483+kgnelnet@users.noreply.github.com>
2024-06-11 11:26:22 -0600

    Update expect_column_values_to_be_null.sql
    Adding query context vars

--- macros/schema_tests/column_values_basic/expect_column_values_to_be_null.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_null.sql
@@ -1,11 +1,12 @@
-{% test expect_column_values_to_be_null(model, column_name, row_condition=None) %}
+{% test expect_column_values_to_be_null(model, column_name, query_context=None, row_condition=None) %}
 
 {% set expression = column_name ~ " is null" %}
 
 {{ dbt_expectations.expression_is_true(model,
                                         expression=expression,
                                         group_by_columns=None,
-                                        row_condition=row_condition
+                                        row_condition=row_condition,
+                                        query_context=query_context
                                         )
                                         }}
 {% endtest %}
