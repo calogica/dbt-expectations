677874a46c977a5626f47fcfc8e4dad510474f74
kgnelnet <166437483+kgnelnet@users.noreply.github.com>
2024-06-11 11:35:35 -0600

    Update expect_column_values_to_be_unique.sql
    add query context vars

--- macros/schema_tests/column_values_basic/expect_column_values_to_be_unique.sql
+++ macros/schema_tests/column_values_basic/expect_column_values_to_be_unique.sql
@@ -1,3 +1,7 @@
-{% test expect_column_values_to_be_unique(model, column_name, row_condition=None) %}
-{{ dbt_expectations.test_expect_compound_columns_to_be_unique(model, [column_name], row_condition=row_condition) }}
+{% test expect_column_values_to_be_unique(model, column_name, row_condition=None, query_context=None) %}
+{{ dbt_expectations.test_expect_compound_columns_to_be_unique(model, 
+                                                                [column_name], 
+                                                                row_condition=row_condition,
+                                                                query_context=query_context
+                                                                ) }}
 {% endtest %}
